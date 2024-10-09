429858e54760f84b8f19017a024bdfe1af3cee1a
Moi042820 <147565177+Moi042820@users.noreply.github.com>
2024-10-09 03:58:36 -0500

    Hice la pantalla de inicio_recurso_humano

---
+++ views/inicio_recursos_humanos.php
@@ -0,0 +1,18 @@
+<?php require("../template/header.php") ?>
+
+<!-- Contenido principal -->
+<section class="container mt-5">
+    <h1 class="text-center">Gestión de Recursos Humanos</h1>
+    <div class="row my-4 d-flex justify-content-center">
+    <div class="col-md-4 mb-4 d-flex align-items-stretch">
+            <div class="card text-center">
+                <div class="card-body">
+                    <p class="card-text">Consulta y genera reportes del sistema.</p>
+                    <a href="/views/registrar_medico.php" class="btn btn-success mb-3">Registro/Verificación de perfil médico</a>
+                    <a href="#" class="btn btn-success mb-3">Gestión de turnos y asignación de personal</a>
+                </div>
+            </div>
+        </div>
+</section>
+
+<?php require("../template/footer.php") ?>
