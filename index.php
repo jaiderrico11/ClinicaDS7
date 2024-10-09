765a44af94a02f97c0f754f0871c2553c3e0060e
quexopacode <luismurcia0106@gmail.com>
2024-10-09 11:33:10 -0500

    Verificacion de session en index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php require("./template/header.php") ?>
 
+<?php
+session_start();
 
+
+if (!isset($_SESSION['usuario'])) {
+    header("Location: views/login.php");
+    exit();
+}
+?>
 
 <?php require("./template/footer.php") ?>
